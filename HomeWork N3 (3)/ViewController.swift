ea45423f5761361bbfc7a8afebae1fe5da0bbe59
Magdiel <magdiel.altynbekov@gmail.com>
2022-12-14 12:07:46 +0600

    my husband bought me this bag

--- HomeWork N3 (3)/ViewController.swift
+++ HomeWork N3 (3)/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var adressTableView: UITableView!
+    
+    var adresses: [String] = ["Нурик - 7 мкр 43дом 55кв","Байгелди - 5 мкр 34дом 13кв","Ислам - 7 мкр 38дом 79кв","Нурсултан - восток-5 55дом 55кв","руслан - 4444444444444444444космос дордой"]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +31,19 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return adresses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = adressTableView.dequeueReusableCell(withIdentifier: "italiano", for: indexPath)
+        
+        cell.textLabel?.text = adresses[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
